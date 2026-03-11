838793433fbe36d5eafb1188c185f2ed13976fc2
jeeva <jeevaprakashdr@gmail.com>
2026-03-11 16:18:01 +0100

    implement print and println macro

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     // vga_buffer::print_something();
 
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers {} {}", 12,2.34).unwrap();
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(vga_buffer::WRITER.lock(), ", some numbers {} {}", 12, 2.34).unwrap();
+
+    println!("Hello world{}", "!");
     loop { }
 }
