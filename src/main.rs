736af1676ff5ec7315342a7b59e67bac9a966e46
jeeva <jeevaprakashdr@gmail.com>
2026-03-11 14:22:42 +0100

    write message to vga screen. Use spinlock

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
+
+use core::{fmt::Write, panic::PanicInfo};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop { }
 }
-static HELLO: &[u8] = b"Hello world!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // vga_buffer::print_something();
+
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers {} {}", 12,2.34).unwrap();
     loop { }
 }
