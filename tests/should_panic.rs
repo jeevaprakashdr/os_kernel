ef2381a6e1488b531a5e7c82922adf7cd85fc5ea
jeeva <jeevaprakashdr@gmail.com>
2026-03-25 17:12:37 +0100

    should panic tests

---
+++ tests/should_panic.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use os_kernel::{exit_qemu, serial_println, serial_print};
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(os_kernel::QemuExitCode::Success);
+    loop {}
+}
+
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+        serial_println!("[test did not panic]");
+        exit_qemu(os_kernel::QemuExitCode::Failed);
+    }
+
+    exit_qemu(os_kernel::QemuExitCode::Success);
+}
+
+#[test_case]
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
+}
