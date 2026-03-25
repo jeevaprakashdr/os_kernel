253cff9994e5add206518180781d26d8f44c5cec
jeeva <jeevaprakashdr@gmail.com>
2026-03-25 17:35:02 +0100

    simplify panic  tests

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,16 +1,14 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use os_kernel::{exit_qemu, serial_println, serial_print};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    test_main();
-
+    should_fail();
+    serial_println!("[tests did not panic]");
+    exit_qemu(os_kernel::QemuExitCode::Failed);
     loop {}
 }
 
@@ -21,18 +19,6 @@ fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(os_kernel::QemuExitCode::Failed);
-    }
-
-    exit_qemu(os_kernel::QemuExitCode::Success);
-}
-
-#[test_case]
 fn should_fail() {
     serial_print!("should_panic::should_fail...\t");
     assert_eq!(0, 1);
