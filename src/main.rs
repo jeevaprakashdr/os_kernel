061ae0aa4ebbfd1db411b6236ee18c96386d7bc9
jeeva <jeevaprakashdr@gmail.com>
2026-03-25 16:58:11 +0100

    remove commented code

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,6 @@ use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    // vga_buffer::print_something();
-    
-    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    // write!(vga_buffer::WRITER.lock(), ", some numbers {} {}", 12, 2.34).unwrap();
-    
     println!("Hello world{}", "!");
     
     #[cfg(test)]
