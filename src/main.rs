1b99916a1f72695ef299b8859f65178186a9c8ef
jeeva <jeevaprakashdr@gmail.com>
2026-03-25 16:44:57 +0100

    abstract test runner from main to lib

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os_kernel::test_runner)]
 #![reexport_test_harness_main = "invoke_all_tests"]
 
 mod vga_buffer;
 mod serial;
 
 use core::panic::PanicInfo;
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    // vga_buffer::print_something();
+    
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(vga_buffer::WRITER.lock(), ", some numbers {} {}", 12, 2.34).unwrap();
+    
+    println!("Hello world{}", "!");
+    
+    #[cfg(test)]
+    invoke_all_tests();
+    
+    loop {}
+}
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -19,68 +35,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[Failed]\n");
-    serial_println!("[Error: {}]\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    // vga_buffer::print_something();
-
-    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    // write!(vga_buffer::WRITER.lock(), ", some numbers {} {}", 12, 2.34).unwrap();
-
-    println!("Hello world{}", "!");
-    
-    #[cfg(test)]
-    invoke_all_tests();
-
-    loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T 
-where T: Fn(),
-{
-    fn run(&self) -> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+    os_kernel::test_panic_handler(info)
 }
